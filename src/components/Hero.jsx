17cc9345f5cfbbce5dda52097bbdf49ce98b0d75
jatin-rajput563 <jkt3298@gmail.com>
2025-07-26 12:51:15 +0530

    work-on-hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import logoImg from "../assets/images/png/logo-text.png";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper/modules";
+import { HeroSlider_Data } from "../utils/helper";
+
 const Hero = () => {
   return (
     <>
-      <div className="min-h-screen">
+      <div className="min-h-screen py-[200px]">
         <div className="max-w-[1440px] px-[37px] bg-light-grey mx-auto rounded-[30px] pt-[77px]">
           <div className="justify-center items-center flex">
             <p className="bg-light-purple max-w-[352.97px] w-full py-[11.5px] rounded-[21px] border border-purple-blue text-purple-blue font-bold leading-100 text-center">
@@ -22,15 +29,42 @@ const Hero = () => {
                 />
               </span>
             </h1>
-            <p className="leading-[129%] opacity-80 text-dark-blue max-w-[820px] text-center mx-auto pt-[15px]">
+            <p className="leading-[129%] opacity-80 text-dark-blue max-w-[820px] text-center pt-[15px] mx-auto">
               Pierwszy w Polsce system CRM oparty na sztucznej inteligencji,
-              stworzony specjalnie dla firm zarządzających nieruchomościami. <br />   
+              stworzony specjalnie dla firm zarządzających nieruchomościami.{" "}
+              <br />
               Zautomatyzuj swoje działania, wyeliminuj chaos i skaluj
               działalność — bez zatrudniania dodatkowych pracowników.
             </p>
-            <div>
-              
-            </div>
+          </div>
+          <div className="mt-10">
+            <Swiper
+              slidesPerView={5}
+              spaceBetween={30}
+              loop={true}
+              pagination={{
+                clickable: false,
+              }}
+              className="mySwiper"
+            >
+              {HeroSlider_Data.map((slide, index) => (
+                <div key={index}>
+                  <SwiperSlide>
+                    <div
+                      className="bg-white rounded-t-[15px] max-w-[231px] w-full h-[261px]"
+                      style={{ boxShadow: "7px -5px 35.4px 0px #5E13F61A" }}
+                    >
+                      <img
+                        className="max-w-[231px] w-full rounded-t-[15px]"
+                        src={slide.Image}
+                        alt="slider-img"
+                      />
+                      <p className="text-xs max-w-[183px] leading-[129%] pt-[31px] pl-[17px] pr-[31px] pb-[40px] text-center mx-auto ">{slide.description}</p>
+                    </div>
+                  </SwiperSlide>
+                </div>
+              ))}
+            </Swiper>
           </div>
         </div>
       </div>
---
+++ src/utils/helper.js
@@ -0,0 +1,37 @@
+import sliderImg1 from "../assets/images/png/slider-img-1.png";
+import sliderImg2 from "../assets/images/png/slider-img-2.png";
+import sliderImg3 from "../assets/images/png/slider-img-3.png";
+import sliderImg4 from "../assets/images/png/slider-img-4.png";
+import { sliderIcon1, sliderIcon2, sliderIcon3, sliderIcon4 } from "./icon";
+export const HeroSlider_Data = [
+    {
+        Image: sliderImg1,
+        icon: sliderIcon1,
+        description: "Twoje automatyczne faktury, obliczenia depozytów, dochody i obsługa zadłużenia",
+    },
+    {
+        Image: sliderImg2,
+        icon: sliderIcon2,
+        description: "Wszystkie informacje o Twoich mieszkaniach, domach, pokojach i kawalerkach",
+    },
+    {
+        Image: sliderImg3,
+        icon: sliderIcon3,
+        description: "Wszystko, czego potrzebujesz, aby zapewnić najlepszą obsługę swoim partnerom.",
+    },
+    {
+        Image: sliderImg4,
+        icon: sliderIcon4,
+        description: "Wszystko, czego potrzebujesz, aby zarządzać wszystkimi najemcami i zapewnić im bardziej komfortowy pobyt.",
+    },
+    {
+        Image: sliderImg2,
+        icon: sliderIcon2,
+        description: "Wszystkie informacje o Twoich mieszkaniach, domach, pokojach i kawalerkach",
+    },
+    {
+        Image: sliderImg3,
+        icon: sliderIcon3,
+        description: "Wszystko, czego potrzebujesz, aby zapewnić najlepszą obsługę swoim partnerom.",
+    },
+]
